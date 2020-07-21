5f3677fb93da9e93486ce50993f4f87018248007
wyhinton <carolinabmx11@gmail.com>
2020-07-21 18:29:39 -0400

    created seperate nav bar style and script docs, ui refinements, added meta tags, removed several excess scripts

--- public/navbar.js
+++ public/navbar.js
@@ -1,12 +1,28 @@
-$('.pageLink').each(function(){
-  console.log(this.id);
-  if (this.id !== 'activePageSection') {
-    $(this).hover(
-       function() {
-         $( this ).css('text-decoration', 'underline');
-       }, function() {
-         $( this ).css('text-decoration', 'none')
-       }
-     );
+// $('.pageLink').each(function(){
+//   console.log(this.id);
+//   if (this.id !== 'activePageSection') {
+//     $(this).hover(
+//        function() {
+//          $( this ).css('text-decoration', 'underline');
+//        }, function() {
+//          $( this ).css('text-decoration', 'none')
+//        }
+//      );
+//   }
+// })
+var mobileWidth = 768
+var hamburgerVisible = false;
+function showHamburgerNavSection() {
+  console.log('hamburger clicked');
+
+  if (window.innerWidth < mobileWidth && hamburgerVisible == false) {
+    $('#navBarListContainer').animate({'right': '0%'})
   }
-})
+
+
+  if (window.innerWidth < mobileWidth && hamburgerVisible == true) {
+    $('#navBarListContainer').animate({'right': '-100%'})
+  }
+  hamburgerVisible = !hamburgerVisible
+
+}
