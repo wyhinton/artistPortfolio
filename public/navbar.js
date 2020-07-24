642781fc6a981b665625410e41531678bc84dbde
wyhinton <carolinabmx11@gmail.com>
2020-07-24 02:11:59 -0400

    mobile ui improvements, synced navbar across pages, background blur out effects in project nav,script segementation, cleanup

--- public/navbar.js
+++ public/navbar.js
@@ -1,28 +1,19 @@
-// $('.pageLink').each(function(){
-//   console.log(this.id);
-//   if (this.id !== 'activePageSection') {
-//     $(this).hover(
-//        function() {
-//          $( this ).css('text-decoration', 'underline');
-//        }, function() {
-//          $( this ).css('text-decoration', 'none')
-//        }
-//      );
-//   }
-// })
+// var navButton = document.getElementById('navButton');
+// navButton.addEventListener('mouseup', showHamburgerNavSection)
+
 var mobileWidth = 768
 var hamburgerVisible = false;
 function showHamburgerNavSection() {
   console.log('hamburger clicked');
 
-  if (window.innerWidth < mobileWidth && hamburgerVisible == false) {
-    $('#navBarListContainer').animate({'right': '0%'})
+  if (w < mobileWidth && hamburgerVisible == false) {
+    $('#navBarListContainer').animate({'right': '0%'}, 500, 'easeOutCirc')
+    $('#innerNavButtonCircle').animate({height: 13, width: 13}, 500, 'easeOutQuad')
   }
 
-
-  if (window.innerWidth < mobileWidth && hamburgerVisible == true) {
+  if (w < mobileWidth && hamburgerVisible == true) {
     $('#navBarListContainer').animate({'right': '-100%'})
+    $('#innerNavButtonCircle').animate({height: 9, width: 9}, 500, 'easeOutSine')
   }
   hamburgerVisible = !hamburgerVisible
-
 }
